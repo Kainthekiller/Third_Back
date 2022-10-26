5cecfb00a53d62e30195332f02694574ba2fa535
kainthekiller <phillipyates12@hotmail.com>
2022-10-26 16:18:06 -0500

    Just need to finish testing logic

--- src/main/java/crud_Three/crud_Three/Controller/ListController.java
+++ src/main/java/crud_Three/crud_Three/Controller/ListController.java
@@ -1,4 +1,83 @@
 package crud_Three.crud_Three.Controller;
 
+import crud_Three.crud_Three.Model.ListModel;
+import crud_Three.crud_Three.Repo.ListRepo;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/List")
 public class ListController {
+    ListRepo listRepo;
+
+    ListController(ListRepo listRepo) {
+        this.listRepo = listRepo;
+    }
+
+    //GET All
+    @GetMapping("")
+    List<ListModel> getAllList()
+    {
+        return listRepo.findAll();
+    }
+
+    //GET ONE
+    @GetMapping("{id}")
+    ResponseEntity<ListModel> getSingleList(@PathVariable Long id)
+    {
+            if (listRepo.findById(id).isPresent())
+            {
+                return new ResponseEntity<>(listRepo.findById(id).get(), HttpStatus.OK);
+            }
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    //PATCH
+    @PatchMapping("{id}")
+    ResponseEntity<ListModel> patchList(@PathVariable Long id, @RequestBody Map<String, String> body)
+    {
+            if (listRepo.findById(id).isPresent())
+            {
+                ListModel holder = listRepo.findById(id).get();
+
+                for (Map.Entry<String, String> entry: body.entrySet())
+                {
+                    switch (entry.getKey())
+                    {
+                        case "Title" -> holder.setTitle(entry.getValue());
+                        case "Content" -> holder.setContent(entry.getValue());
+                    }
+                }
+                listRepo.save(holder);
+                return new ResponseEntity<>(listRepo.findById(id).get(), HttpStatus.OK);
+            }
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    //POST
+    @PostMapping("")
+    ResponseEntity<ListModel> postList(@RequestBody ListModel body)
+    {
+           return new ResponseEntity<>(listRepo.save(body), HttpStatus.OK);
+    }
+
+    //DELETE
+    @DeleteMapping("{id}")
+    ResponseEntity<ListModel> deleteList(@PathVariable Long id)
+    {
+        if (listRepo.findById(id).isPresent())
+        {
+            listRepo.deleteById(id);
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+
 }
--- src/main/java/crud_Three/crud_Three/Model/ListModel.java
+++ src/main/java/crud_Three/crud_Three/Model/ListModel.java
@@ -1,4 +1,57 @@
 package crud_Three.crud_Three.Model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.NotNull;
+import crud_Three.crud_Three.Repo.ListRepo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class ListModel {
+
+
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @JsonProperty("Title")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    String title;
+
+    @JsonProperty("Content")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    String content;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
